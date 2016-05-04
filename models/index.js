5ae70cd5261cd35557caeff2e274f894715869b0
daniel guantes <danielmguantes@gmail.com>
2016-05-04 13:30:18 +0200

    varias preguntas

--- models/index.js
+++ models/index.js
@@ -1,26 +1,42 @@
-var path=require('path');
+var path = require('path');
 
-//cargar modelo ORM
-var Sequelize=require('sequelize');
+// Cargar ORM
+var Sequelize = require('sequelize');
 
-//usar BBDD SQLite
-var sequelize= new Sequelize(null,null,null,{dialect:"sqlite", storage:"quiz.sqlite"});
+var url, storage;
+if (!process.env.DATABASE_URL) {
+    url = "sqlite:///";
+    storage = "quiz.sqlite";
+} else {
+    url = process.env.DATABASE_URL;
+    storage = process.env.DATABASE_STORAGE || "";
+}
+var sequelize = new Sequelize(url, 
+	 						  { storage: storage,
+				              	omitNull: true 
+				              });
 
-//importar la def. de la tabla de models/quiz.js
-var Quiz=sequelize.import(path.join(__dirname,'quiz'));
+// Importar la definicion de la tabla Quiz de quiz.js
+var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 
 
-//sequelize.sync crea e inicializa la tabla de preguntas en db
-sequelize.sync().then(function(){
-	return Quiz.count().then(function(c){ //count cuenta las filas de la tabla, cuando termina las pasa a then como param
-		if(c===0){//tabla vacia
-			return Quiz.create({question: 'Capital de Italia',answer: 'Roma'})//inicializamos la tabla con una pregunta
-			.then(function(){console.log('Base de datos inicializada con datos');});
-		}
-	});
-}).catch(function(error){
-	console.log("Error sincronizando las tablas de la BBDD:", error);
-	process.exit(1);
-});
-
-exports.Quiz=Quiz;//exportar definicion de la tabla Quiz
+// sequelize.sync() crea e inicializa tabla de preguntas en DB
+sequelize.sync()
+    .then(function() {
+        // Ya se han creado las tablas necesarias.
+        return Quiz.count()
+                .then(function (c) {
+                    if (c === 0) {   // la tabla se inicializa solo si está vacía
+                        return Quiz.bulkCreate([ {question: 'Capital de Italia',   answer: 'Roma'},
+                                                 {question: 'Capital de Portugal', answer: 'Lisboa'}
+                                              ])
+                                   .then(function() {
+                                        console.log('Base de datos inicializada con datos');
+                                    });
+                    }
+                });
+    })
+    .catch(function(error) {
+        console.log("Error Sincronizando las tablas de la BBDD:", error); process.exit(1);
+    });
+exports.Quiz = Quiz; // exportar definición de tabla Quiz
