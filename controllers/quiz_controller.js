65ce02e0bc51c74da1ae90af4fc830fb3bdeed0c
daniel guantes <danielmguantes@gmail.com>
2016-05-04 13:17:28 +0200

    BD heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -3,7 +3,7 @@ var models=require('../models');
 //GET /question
 exports.question=function(req,res,next){
 	models.Quiz.findOne()
-	.then(function(quiz){
+	.then(function(quiz){//con findOne busca la primera pregunta
 		if(quiz){
 			var answer=req.query.answer || '';
 			res.render('quizzes/question',{question:quiz.question, answer:answer});
@@ -14,21 +14,16 @@ exports.question=function(req,res,next){
 };	
 
 //GET /check
-
-
-
-
-
-
-
-exports.question=function(req,res,next){
-	var answer= req.query.answer || '';
-	res.render('quizzes/question',{question:'Capital de Italia',answer: answer})
-}
-
-
 exports.check=function(req,res,next){
-	var answer= req.query.answer || '';
-	var result=req.query.answer== 'Roma' ? 'Correcta' : 'Incorrecta';
-	res.render('quizzes/result',{result:result, answer: answer});
-}
+	models.Quiz.findOne().then(function(quiz){//con findOne busca la primera pregunta
+		if(quiz){
+			var answer=req.query.answer || '';
+			var result=answer===quiz.answer ? 'Correcta' : 'Incorrecta';
+			res.render('quizzes/result',{result: result, answer: answer});
+		}else{
+			throw new Error('No hay preguntas en la BBDD.');
+		}
+	}).catch(function(error){next(error);});//llama al MW de error
+};
+
+
