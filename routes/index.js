7513cb0fb08df18847daa8a147eeefabc8af3ff7
daniel guantes <danielmguantes@gmail.com>
2016-06-01 18:54:23 +0200

    gestion users

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 
 var quizController=require('../controllers/quiz_controller');
 var commentController=require('../controllers/comment_controller');
+var userController=require('../controllers/user_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,8 +12,11 @@ router.get('/', function(req, res, next) {
 });
 
 router.param('format',quizController.format);
-//Autoload
+
+//Autoload quiz
 router.param('quizId',quizController.load);//cuando quizId existe
+//Autoload user
+router.param('userId',userController.load);//cuando userId existe
 
 //rutas /quizzes
 router.get('/quizzes.:format?', quizController.index);
@@ -27,6 +31,19 @@ router.delete('/quizzes/:quizId(\\d+)',quizController.destroy);
 //rutas comments
 router.get('/quizzes/:quizId(\\d+)/comments/new',commentController.new);
 router.post('/quizzes/:quizId(\\d+)/comments',commentController.create);
+
+
+//rutas users
+router.get('/users',userController.index);
+router.get('/users/:userId(\\d+)',userController.show);
+router.get('/users/new',userController.new);
+router.post('/users',userController.create);
+router.get('/users/:userId(\\d+)/edit',userController.edit);
+router.put('/users/:userId(\\d+)',userController.update);
+router.delete('/users/:userId(\\d+)',userController.destroy);
+
+
+
 
 
 router.get('/author',function(req, res, next) { res.render('author');});
